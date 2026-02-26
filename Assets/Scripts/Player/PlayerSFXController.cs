e453ceb33fe5d643d816de25bdbd3837c86f8fee
ricxi <irixoc@gmail.com>
2026-02-26 04:48:33 -0500

    set everything up for sound, but shift seems to toggle on and off the sound for player movement

--- Assets/Scripts/Player/PlayerSFXController.cs
+++ Assets/Scripts/Player/PlayerSFXController.cs
@@ -3,7 +3,6 @@ using UnityEngine.InputSystem;
 
 public class PlayerSFXController : MonoBehaviour
 {
-    [Header("Refs")]
     [SerializeField] private PlayerInput playerInput;
 
     private InputAction fireAction;
@@ -14,7 +13,6 @@ public class PlayerSFXController : MonoBehaviour
         if (playerInput == null) playerInput = GetComponentInParent<PlayerInput>();
         fireAction = playerInput.actions["Attack"];
         moveAction = playerInput.actions["Rolling/Move"];
-        // moveAction = GetComponent<PlayerInput>().actions["Rolling/Move"];
     }
 
     private void OnEnable()
@@ -38,18 +36,19 @@ public class PlayerSFXController : MonoBehaviour
             Debug.LogError("AudioManager instance not found");
             return;
         }
+
         AudioManager.Instance.PlayOneShot(SoundType.Gunshot);
     }
 
+    // Pressing shift seems to toggle on and off the sound for movement
     private void OnMoveStartPlaySound(InputAction.CallbackContext ctx)
     {
-        Debug.LogError("OnMoveStartCalled");
         if (AudioManager.Instance == null)
         {
             Debug.LogError("AudioManager instance not found");
             return;
         }
-        Debug.LogError("OnMoveStartCalled 2");
+
         AudioManager.Instance.Play(SoundType.Move);
     }
 
@@ -61,7 +60,6 @@ public class PlayerSFXController : MonoBehaviour
             return;
         }
 
-        Debug.LogError("OnMoveStopCalled");
         AudioManager.Instance.Stop();
     }
 }
