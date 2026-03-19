07ee2c1e08c6b97e75ee1d1553717abe064ffb5d
ricxi <irixoc@gmail.com>
2026-03-19 06:43:59 -0400

    finished health ui: health bar updates when player's hp changes

--- Assets/Scripts/Player/PlayerHealthController.cs
+++ Assets/Scripts/Player/PlayerHealthController.cs
@@ -1,20 +1,31 @@
+using System;
 using UnityEngine;
 
 public class PlayerHealthController : MonoBehaviour, IDamageable
 {
+    public event Action<float, float> OnHealthChanged; // This is used by the PlayerUIHandler/Manager to update the health bar.
+
     [SerializeField] private float maxHealth = 100f;
-    private float currentHealth;
+    [SerializeField] private FlashDamageFX flashDamageFX; // Makes player appear red when damaged (you can delete this; I was only really using it for testing)
+
+    private float _currentHealth;
 
     private void Awake()
     {
-        currentHealth = maxHealth;
+        _currentHealth = maxHealth;
+
+        OnHealthChanged?.Invoke(_currentHealth, maxHealth);
     }
 
     public void TakeDamage(float amount, Vector3 hitPoint, Vector3 hitDirection)
     {
-        currentHealth -= amount;
+        _currentHealth -= amount;
 
-        if (currentHealth <= 0f)
+        flashDamageFX.Play(); // Flash red when damaged (you can delete this; was just for testing)
+
+        OnHealthChanged?.Invoke(_currentHealth, maxHealth); // Update health bar again
+
+        if (_currentHealth <= 0f)
         {
             Die();
         }
--- Assets/Scripts/VFX/FlashDamageFX.cs
+++ Assets/Scripts/VFX/FlashDamageFX.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 // [RequireComponent(typeof(MeshRenderer))]
 // [RequireComponent(typeof(Material))]
-public class FlashDamageFX : MonoBehaviour, IDamageable
+public class FlashDamageFX : MonoBehaviour
 {
     [SerializeField] private MeshRenderer meshRenderer;
     [SerializeField] private Material flashDamageMaterial;
@@ -17,17 +17,14 @@ public class FlashDamageFX : MonoBehaviour, IDamageable
         _originalMaterial = meshRenderer.material;
     }
 
-    // Not the greatest idea in the world, but we'll change this later.
-    public void TakeDamage(float amount, Vector3 hitPoint, Vector3 hitDirection)
+    public void Play()
     {
-        Debug.Log("Called: 23");
         if(_playFlashCoHandler != null)
         {
             StopCoroutine(_playFlashCoHandler);
             _playFlashCoHandler = null;
         }
 
-        Debug.Log("Called: 30");
         _playFlashCoHandler = StartCoroutine(PlayFlash());
     }
 
